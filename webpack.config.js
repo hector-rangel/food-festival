1a3c3e806daffcc8633ae56c2898e383a7450de4
Hector Rangel <hector.rangeljr@yahoo.com>
2021-07-22 18:31:03 -0500

    add pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack")
 const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: {
@@ -14,7 +15,7 @@ module.exports = {
     path: __dirname + "/dist",
   },
   module: {
-    rules: [ 
+    rules: [
       {
         test: /\.jpg$/i,
         use: [
@@ -22,11 +23,11 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name (file) {
+              name(file) {
                 return "[path][name].[ext]"
               },
-              publicPath: function(url) {
-                return url.replace("../","/assets/")
+              publicPath: function (url) {
+                return url.replace("../", "/assets/")
               }
             }
           },
@@ -38,13 +39,28 @@ module.exports = {
     ]
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-  })
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
   ],
   mode: "development"
 };
